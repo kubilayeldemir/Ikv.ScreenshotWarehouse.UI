d03069275dbc8f149ad9c2745e50ed329526cde7
kubilayeldemir <kubi0707@gmail.com>
2022-02-05 16:21:54 +0300

    Removed tutorial page - bg color and about page added

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-dark text-white">
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default",
+  head() {
+    return {
+      title:"İstanbul Kıyamet Vakti SS Deposu",
+      meta:[
+        {
+          hid: "İkv SS Deposu",
+          name: "İkv SS Deposu",
+          content: "İstanbul Kıyamet Vakti oyununun instagramı"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-dark">
+    <b-container>
+      <b-row class="vh-100 text-center" align-v="center">
+        <b-col></b-col>
+        <b-col>
+          <div class="text-capitalize text-monospace shadow-lg bg-dark-50">
+            Ikv SS Deposu <br>
+            Made with ❤
+          </div>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+
+</style>
