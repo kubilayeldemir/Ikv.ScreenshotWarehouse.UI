9176f701c9e3f55794b22bde182cc71a95d2b26d
kubilayeldemir <kubi0707@gmail.com>
2022-02-06 18:47:59 +0300

    postCard lightGallery css update

--- pages/Posts/index.vue
+++ pages/Posts/index.vue
@@ -3,8 +3,9 @@
     <no-ssr>
       <LightGallery :images="postsNew" :index="index" @close="index = null"></LightGallery>
       <Stack :monitor-images-loaded="true" :column-min-width="320" :gutter-width="4" :gutter-height="4">
-        <StackItem v-for="(post,i) in posts" :key="i" @click.native="index=i">
-          <post-card :post="post"></post-card>
+        <StackItem v-for="(post,i) in posts" :key="i">
+          <post-card :post="post" @click.native="index=i"></post-card>
+          <div class="w-100 p-0" style="background-color: red"><small class="float-left">{{post.username}}</small> <small class="float-right">{{post.screenshotDate}}</small> </div>
         </StackItem>
       </Stack>
     </no-ssr>
@@ -31,7 +32,7 @@ export default {
       try {
         const res = await api.get('/post/all');
         this.posts = res.data
-        this.postsNew = res.data.map(x => ({url: x.fileUrl, title: "asdasd", h: "768"}))
+        this.postsNew = res.data.map(x => ({url: x.fileUrl, title: "asdasd", h: "1200"}))
       } catch (e) {
         console.log(e)
       }
@@ -42,4 +43,12 @@ export default {
 
 <style scoped>
 
+</style>
+<style>
+.light-gallery__text{
+  display: none !important;
+  padding: 0px !important;
+  width: 30% !important;
+  font-size: 1em !important;
+}
 </style>
